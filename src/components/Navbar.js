fea949e27ca90589e0e2ba983eb4489121a947db
ojasvik <khuranaojasvi@gmail.com>
2021-07-26 12:14:04 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import UserImage from '../images/user.png';
 import Logo from '../images/netflix.svg';
+import {Link} from 'react-router-dom';
+
 export default function NavBar() {
     return (
         <Navbar>
   <Container>
     <Navbar.Brand>
+        <Link to="/">
         <img
             src={Logo}
             width="30"
@@ -14,9 +17,11 @@ export default function NavBar() {
             className="d-inline-block align-top"
             alt="Netflix logo"
         />
+        </Link>
     </Navbar.Brand>
     <Nav>
     <Navbar.Brand>
+        <Link to="/profile">
         <img
             src={UserImage}
             width="30"
@@ -24,6 +29,8 @@ export default function NavBar() {
             className="d-inline-block align-top"
             alt="User Profile"
         />
+        </Link>
+        
     </Navbar.Brand>
     </Nav>    
       
